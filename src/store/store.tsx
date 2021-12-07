2ac708639c52668d4dfb8d1ca3635f34a9be7d40
jordaniza <jordan.imran@predixa.ai>
2021-12-07 20:21:59 +0400

    type changes

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -12,20 +12,20 @@ export interface KPIs {
   MIN_MCAP:                number;
   MAX_MCAP:                number;
   AVG_MCAP:                number;
-  originalRATIO:           number;
-  RATIO:                   number;
+  originalRATIO?:           number;
+  RATIO:                   number | string;
   initialAmounts:          number;
-  CAPPED:                  boolean;
-  ADJUSTED:                boolean;
-  relativeToLeftoverRATIO: string;
-  adjustedMarketCAP:       string;
-  addedRatio:              string;
-  adjustedRATIO:           string;
-  VARIANCE:                number;
-  STDEV:                   number;
-  marginalContribution:    number;
-  totalContribution:       number;
-  MCTR:                    number;
+  CAPPED?:                  boolean;
+  ADJUSTED?:                boolean;
+  relativeToLeftoverRATIO?: string;
+  adjustedMarketCAP?:       string;
+  addedRatio?:              string;
+  adjustedRATIO?:           string;
+  VARIANCE?:                number;
+  STDEV?:                   number;
+  marginalContribution?:    number;
+  totalContribution?:       number;
+  MCTR?:                    number;
   tokenBalance:            number;
 }
 
